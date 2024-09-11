a4fd52ebf7815b467c7acea76730f7d58ab19ed0
hhhannahmmmontana <rubakhin.edu@niuitmo.ru>
2024-09-11 13:31:38 +0300

    initial commit

---
+++ lib/хайп++.h
@@ -0,0 +1,89 @@
+﻿#ifndef абсолют_база
+#define абсолют_база
+
+#ifdef _WIN32
+#include <Windows.h>
+#define на_хайпе SetConsoleOutputCP(1251)
+#else
+#include <unistd.h>
+#define на_хайпе
+#endif
+
+#include <fstream>
+#include <iostream>
+
+
+#define моргенштерн main
+#define юзать using
+#define тэггинг typedef
+#define фортнайт std
+#define чуитс cout
+#define засейвить cin
+#define кринж return
+
+#define флексить do
+#define флексить_антил while
+#define фор for
+
+#define чи if
+#define не_чи else
+
+#define чел class
+#define микрочел struct
+#define кэш enum
+
+#define ливать break
+#define прода continue
+#define барак switch
+#define обама case
+#define ну_типо default
+
+#define трустори true
+#define наебал_мамонта false
+
+#define база ;
+#define типо typeid
+#define челики endl
+#define дроп new
+#define слить delete
+#define ниче nullptr
+#define НИЧЕ NULL
+#define рандом random
+#define по_факту const
+#define трэшить throw
+#define йоу exception
+#define палить_тикток ifstream
+#define снять_тикток ofstream
+#define подкаст fstream
+#define бро friend
+#define пиздуй goto
+#define по_факту template
+#define че_то_типо typename
+#define личная_беседа private
+#define тг_канал public
+#define тг_канал_для_мамы protected
+#define бревно inline
+#define доска static
+#define без_кринжа unsigned
+#define заскамить try
+#define орнуть catch
+#define никнейм namespace
+#define чекнуть open
+#define рейдж_квитнуть close
+#define по_кд auto
+#define короче static_cast
+
+#define бартер std::swap
+
+юзать мани = int;
+юзать дабл = double;
+юзать флуд = float;
+юзать лэттер = char;
+юзать тати = фортнайт::string;
+юзать эмпти = void;
+юзать низкий_флекс = short;
+юзать дикпик = long;
+юзать мой_дикпик = long long;
+юзать чи_не_чи = bool;
+
+#endif
